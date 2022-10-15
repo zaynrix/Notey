92ad11bdc99a3ffb7355286c2ee0474bb421a0f6
mac <zayns1914@gmail.com>
2022-10-15 16:29:00 +0300

    update

--- lib/features/Home/homeScreen.dart
+++ lib/features/Home/homeScreen.dart
@@ -224,35 +224,6 @@ class noteCard extends StatelessWidget {
   }
 }
 
-// Widget slideRightBackground() {
-//   return Container(
-//     color: Colors.green,
-//     child: Align(
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.start,
-//         children: <Widget>[
-//           SizedBox(
-//             width: 20,
-//           ),
-//           Icon(
-//             Icons.edit,
-//             color: Colors.white,
-//           ),
-//           Text(
-//             " Edit",
-//             style: TextStyle(
-//               color: Colors.white,
-//               fontWeight: FontWeight.w700,
-//             ),
-//             textAlign: TextAlign.left,
-//           ),
-//         ],
-//       ),
-//       alignment: Alignment.centerLeft,
-//     ),
-//   );
-// }
-
 Widget slideLeftBackground() {
   return Container(
     color: Colors.red,
