463b13f6316d050b59c586e7b35053c26f6361fe
mac <zayns1914@gmail.com>
2022-10-17 09:48:34 +0300

    update

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:notey/routing/routes.dart';
+import 'package:notey/interceptors/di.dart';
+import 'package:notey/utils/appConfig.dart';
+import 'package:notey/features/splashScreen.dart';
 import 'package:notey/features/Home/homeScreen.dart';
+import 'package:notey/features/onBoardingScreen.dart';
+import 'package:notey/features/Settings/typography.dart';
+import 'package:notey/features/Settings/settingScreen.dart';
+import 'package:notey/features/Settings/contactUsScreen.dart';
 import 'package:notey/features/Registrations/loginScreen.dart';
+import 'package:notey/features/Registrations/signUpScreen.dart';
 import 'package:notey/features/Registrations/auth_provider.dart';
 import 'package:notey/features/Registrations/forgetPasswordScreen.dart';
 import 'package:notey/features/Registrations/createNewPasswordScreen.dart';
-import 'package:notey/features/Registrations/signUpScreen.dart';
-import 'package:notey/features/Settings/contactUsScreen.dart';
-import 'package:notey/features/Settings/settingScreen.dart';
-import 'package:notey/features/Settings/typography.dart';
-import 'package:notey/features/onBoardingScreen.dart';
-import 'package:notey/features/splashScreen.dart';
-import 'package:notey/interceptors/di.dart';
-import 'package:notey/routing/routes.dart';
-import 'package:notey/utils/appConfig.dart';
-import 'package:provider/provider.dart';
 
 class RouterX {
   static Route<dynamic> generateRoute(RouteSettings settings) {
 
     switch (settings.name) {
+
+
+    // ------------- Splash Screen ---------------
 
       case Routes.splash:
         return MaterialPageRoute(
@@ -28,10 +31,16 @@ class RouterX {
           ),
         );
 
+
+    // ------------- Introduction Screens ---------------
+
       case Routes.intro:
         return MaterialPageRoute(
           builder: (_) => const Introduction(),
         );
+
+
+    // ------------- Login Screen ---------------
 
       case Routes.login:
         return MaterialPageRoute(
@@ -41,6 +50,9 @@ class RouterX {
           ),
         );
 
+
+    // ------------- Forget Password Screen ---------------
+
       case Routes.forgetPassword:
         return MaterialPageRoute(
           builder: (_) => ChangeNotifierProvider.value(
@@ -49,13 +61,17 @@ class RouterX {
           ),
         );
 
-        case Routes.createNewPassword:
+    // ------------- Create Password Screen ---------------
+
+      case Routes.createNewPassword:
         return MaterialPageRoute(
           builder: (_) => ChangeNotifierProvider.value(
             value: sl<AuthProvider>(),
             child: CreateNewPassword(),
           ),
         );
+
+    // ------------- Signup Screen ---------------
 
       case Routes.signUp:
         return MaterialPageRoute(
@@ -65,21 +81,28 @@ class RouterX {
           ),
         );
 
+    // ------------- Home Screen ---------------
+
       case Routes.home:
         return MaterialPageRoute(
           builder: (_) => HomeScreen(),
         );
 
-        case Routes.setting:
+    // ------------- Settings Screen ---------------
+
+      case Routes.setting:
         return MaterialPageRoute(
           builder: (_) => SettingsScreen(),
         );
+
+    // ------------- Theme Screen ---------------
 
       case Routes.typographyScreen:
         return MaterialPageRoute(
           builder: (_) => TypographyScreen(),
         );
 
+        // ------------- Contact Us Screen ---------------
       case Routes.contactus:
         return MaterialPageRoute(
           builder: (_) => ContactUsScreen(),
