203119eadf5109855b90314fd9cf372946d9828b
mac <zayns1914@gmail.com>
2022-10-19 09:38:24 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Localization
   await EasyLocalization.ensureInitialized();
 
   // Screen Utils Initialize
