709782113772b730bc022020f4ed00718b2577ea
mac <zayns1914@gmail.com>
2022-10-20 13:07:11 +0300

    update

--- lib/features/Settings/settingProvider.dart
+++ lib/features/Settings/settingProvider.dart
@@ -59,7 +59,6 @@ class SettingProvider extends ChangeNotifier {
   }
 
   // ------------------ Change Note ColorIndex  ------------------
-
   changeIndexColor(int index) {
     colorIndex = index;
     sl<SharedLocal>().setColorIndex = index;
